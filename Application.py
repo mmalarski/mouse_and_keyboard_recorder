aa16e56bb02694e867bcbaa607de056ac8e1111c
Michał Malarski <229951@edu.p.lodz.pl>
2024-10-27 15:23:08 +0100

    chore: cleanup

--- Application.py
+++ Application.py
@@ -8,6 +8,9 @@ class Application:
     def __init__(self):
         # self.mouse_recorder = MouseRecorder()
         self.user_input = None
+        self.active_task = None
+        self.session_dir = self.setup_session_directories()
+        self.exit = False
         keyboard.on_press_key("num 1", self.on_num_press)
         keyboard.on_press_key("num 2", self.on_num_press)
         keyboard.on_press_key("num 3", self.on_num_press)
@@ -16,56 +19,63 @@ class Application:
         keyboard.on_press_key("backspace", self.on_num_press)
 
     def run(self):
-        session_dir = self.setup_session_directories()
-        active_task = None  # Track currently active task
 
-        print("Welcome! Press 1, 2, 3, or 4 to start recording a task.")
-        print("Press 0 to stop the current recording.")
-        print("Press 'e' to exit the application.")
+        print("Welcome! Press numpad 1, 2, 3, or 4 to start recording a task.")
+        print("Press numpad 0 to stop the current recording.")
+        print("Press 'backspace' to exit the application.")
 
-        while True:
-            if self.user_input is None:
-                continue
-            elif self.user_input in ["1", "2", "3", "4"]:  # Start recording a new task
-                if active_task is not None:
-                    print(
-                        f"Already recording Task {active_task}. Stop it first with '0'."
-                    )
-                else:
-                    filename = self.get_filename(session_dir, self.user_input)
-                    print(
-                        f"Starting recording for Task {self.user_input}. Filename: {filename}"
-                    )
-                    active_task = self.user_input
-                    # Here you would start your recording code for the selected task
-
-            elif self.user_input == "0":  # Stop current recording
-                if active_task is not None:
-                    print(f"Stopping recording for Task {active_task}.")
-                    active_task = None
-                    self.user_input = None
-                    # Here you would stop your recording code for the active task
-                else:
-                    print("No active recording to stop.")
-
-            elif self.user_input == "backspace":  # Exit the application11
-                if active_task is None:
-                    print("Exiting application.")
-                    break
-                else:
-                    print(
-                        f"Recording is active for Task {active_task}. Stop it first with '0' before exiting."
-                    )
-
-            else:
-                print(
-                    f"Invalid input {self.user_input}. Please enter a task number (1-4), '0' to stop, or 'e' to exit."
-                )
-
-            self.user_input = None
+        while self.exit is False:
+            self.update()
 
     # def cleanup(self):
     # self.mouse_recorder.cleanup()
+
+    def update(self):
+        if self.user_input is None:
+            return
+        elif self.user_input in ["1", "2", "3", "4"]:
+            self.start_recording_task(self.user_input)
+
+        elif self.user_input == "0":
+            self.stop_recording_task()
+
+        elif self.user_input == "backspace":
+            self.terminate()
+
+        else:
+            print(
+                f"Invalid input {self.user_input}. Please enter a task number (numpad 1-4), '0' to stop, or 'backspace' to exit."
+            )
+
+        self.user_input = None
+
+    def start_recording_task(self, task_num):
+        if self.active_task is not None:
+            print(f"Already recording Task {self.active_task}. Stop it first with '0'.")
+            return
+        else:
+            filename = self.get_filename(self.session_dir, task_num)
+            print(f"Starting recording for Task {task_num}. Filename: {filename}")
+            self.active_task = task_num
+            # Here you would start your recording code for the selected task
+
+    def stop_recording_task(self):
+        if self.active_task is not None:
+            print(f"Stopping recording for Task {self.active_task}.")
+            self.active_task = None
+            self.user_input = None
+            # Here you would stop your recording code for the active task
+        else:
+            print("No active recording to stop.")
+
+    def terminate(self):
+        if self.active_task is None:
+            print("Exiting application.")
+            self.exit = True
+        else:
+            print(
+                f"Recording is active for Task {self.active_task}. Stop it first with '0' before exiting."
+            )
 
     def on_num_press(self, num):
         self.user_input = num.name
@@ -87,9 +97,3 @@ class Application:
         return os.path.join(
             session_dir, f"task_{task_num}", f"{dt_str}_task_{task_num}.avi"
         )
-
-    def start_recording_task(self, user_input):
-        self.user_input = user_input
-
-    def stop_recording_task(self):
-        self.user_input = 0
